265ad0e9f2db298880295b4c1a3f59148135cbdd
Mubarak Odufade <91754014+mao-99@users.noreply.github.com>
2023-11-16 13:56:33 -0500

    Updated main.cpp 11/16/2023
    Gotten closer to the final version

--- main.cpp
+++ main.cpp
@@ -31,16 +31,16 @@ void selectionSort(vector <Type>& myVector) {
 template <typename Type>
 int search(vector <Type> myVector, string name) {
 	//This loop runs through the vector and finds players who we are searching for
-	int playerIndex = -1;
+	int playerIndex;
 	for (int i = 0; i < myVector.size(); i++) {
-		bool found = false;
 		if (myVector.at(i) == name) {
-			found = true;
 			playerIndex = i;
-		}
-		if (myVector.at(i) > name) {
 			break;
 		}
+		/*if (myVector.at(i) > name) {
+			playerIndex = -1;
+			break;
+		}*/
 	}
 	return playerIndex;
 }
@@ -70,49 +70,76 @@ int main() {
 	}
 	cout << endl;
 	cout << "This is a check for after reading the players from the file \n";
-	cout << "The vector size is: " << myPlayers.size() << endl;
-	cout << endl;
-	cout << "This is the vector after sorting: " << endl;
+	cout << "The number of players is: " << myPlayers.size() << endl;
 	selectionSort(myPlayers);
 	for (int i = 0; i < myPlayers.size(); i++) {
 		myPlayers.at(i).display();
 	}
 	vector <Team> allTeams;
 	Team firstTeam(myPlayers.at(0).getCountry());
+	firstTeam.add(myPlayers.at(0));
 	allTeams.push_back(firstTeam);
 	for (int i = 1; i < myPlayers.size(); i++) {
 		if (myPlayers.at(i).getCountry() != allTeams.at((allTeams.size() - 1)).getTeamName()) {
 			Team aTeam(myPlayers.at(i).getCountry());
+			aTeam.add(myPlayers.at(i));
 			allTeams.push_back(aTeam);
 		}
-	}
-	for (int i = 0; i < allTeams.size(); i++) {
-		cout << allTeams.at(i).getTeamName() << endl;
-	}
-	/*string userInput;
-	cout << "Type a players name or press n to quit: " << endl;
-	cin >> userInput;
-	while (userInput != "n") {
-		for (int i = 0; i < userInput.length(); i++) {
-			userInput[i] = toupper(userInput[i]);
-		}
-		int val = search(myPlayers, userInput);
-		if (val == -1) {
-			cout << "The player was not found" << endl;
-		}
 		else {
-			myPlayers.at(val).display();
+			allTeams.at((allTeams.size() - 1)).add(myPlayers.at(i));
 		}
-		cout << "Type another players name or press n to quit: " << endl;
-		cin >> userInput;
-
-	}*/
-	Team jpnTeam("JPN");
-	int val = search(myPlayers, "KAROLINA");
-	jpnTeam.add(myPlayers.at(val));
-	jpnTeam.display();
-
+	}
+	cout << "The number of teams: " << allTeams.size() << endl;
 	cout << endl;
-
+	cout << "These are the teams: " << endl;
+	cout << endl;
+	for (int i = 0; i < allTeams.size(); i++) {
+		allTeams.at(i).display();
+	}
+	cout << "Select one: \n 1. Display a team by country code \n 2. Add points to a player using their name and country \n 3. Test the Copy Constructor \n 4. Test the Assignment Operator" << endl;
+	int userInput;
+	cin >> userInput;
+	
+	//Things to work on:
+	//the copy constructor for a team. when a user copies a team and updates the points for one of the players in the team ensure that it only updates the original team and not its copy. Same for the assignment operator.
+	//Use switch cases for the userInput menu instead of using if-statements. Finally, ensure that you ask the user for input in a loop and they choose a menu option to quit.
+	
+	while (userInput < 1 || userInput > 4) {
+		cout << "Please provide a valid selection: ";
+		cin >> userInput;
+	}
+	if (userInput == 1) {
+		cout << "Please provide the country code in all caps: ";
+		string countryCode;
+		cin >> countryCode;
+		for (int i = 0; i < allTeams.size(); i++) {
+			if (allTeams.at(i).getTeamName() == countryCode) {
+				allTeams.at(i).display();
+				break;
+			}
+			if ((i == (allTeams.size() - 1)) && (allTeams.at(i).getTeamName() != countryCode)) {
+				cout << "Invalid Country" << endl;
+			}
+		}
+	}
+	else if (userInput == 2) {
+		string fName, lName, coun;
+		cout << "Please provide the player's first name: ";
+		cin >> fName;
+		cout << "Please provide the player's last name: ";
+		cin >> lName;
+		cout << "Please provide the player's country: ";
+		cin >> coun;
+		for (int i = 0; i < allTeams.size(); i++) {
+			if (allTeams.at(i).getTeamName() == coun) {
+				allTeams.at(i).getPlayer(fName, lName).display();
+				cout << "How many points do you want to add to this player? ";
+				int extraPoints;
+				cin >> extraPoints;
+				allTeams.at(i).updatePlayerPoints(fName, lName, extraPoints);
+				allTeams.at(i).getPlayer(fName, lName).display();
+			}
+		}
+	}
 	return 0;
 }
