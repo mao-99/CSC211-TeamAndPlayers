1fbeae9c0517c11849e67b0383f7ade2d682a1ea
Mubarak Odufade <91754014+mao-99@users.noreply.github.com>
2023-11-16 13:57:52 -0500

    Updated team.cpp

--- team.cpp
+++ team.cpp
@@ -17,11 +17,30 @@ void Team::add(Player aPlayer) {
 	this->mNumberOfPlayers += 1;
 }
 void Team::display() {
-	cout << "The number of players in the team is: " << this->mNumberOfPlayers << " . The players are: \n";
+	cout << "The number of players in the " << mCountry << " team is : " << this->mNumberOfPlayers << ".The players are : \n";
 	for (int i = 0; i < this->teamPlayers->size(); i++) {
 		teamPlayers->at(i).display();
 	}
+	cout << endl;
 }
 string Team::getTeamName() {
 	return this->mCountry;
+}
+int Team::getNumPlayers() {
+	return this->mNumberOfPlayers;
+}
+Player Team::getPlayer(string first, string last) {
+	for (int i = 0; i < this->teamPlayers->size(); i++) {
+		if (this->teamPlayers->at(i).getFirstName() == first && this->teamPlayers->at(i).getLastName() == last) {
+			return this->teamPlayers->at(i);
+		}
+	}
+}
+void Team::updatePlayerPoints(string first, string last, int extraPoints) {
+	for (int i = 0; i < this->teamPlayers->size(); i++) {
+		if (this->teamPlayers->at(i).getFirstName() == first && this->teamPlayers->at(i).getLastName() == last) {
+			int originalPoints = this->teamPlayers->at(i).getPoints();
+			this->teamPlayers->at(i).setPoints(originalPoints + extraPoints);
+		}
+	}
 }
